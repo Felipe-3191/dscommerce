28a89f2a4ec49f894b4e904b38d9a23863c22f64
Felipe Santos <felipe.dss17@gmail.com>
2023-06-10 00:20:47 -0300

    commit inicial desafio 2,estrutura do projeto definida

---
+++ desafio2-modelodominio-orm/dominioorm/src/main/java/com/devsuperior/felipe/dominioorm/DominioormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.felipe.dominioorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioormApplication.class, args);
+	}
+
+}
---
+++ desafio2-modelodominio-orm/dominioorm/src/test/java/com/devsuperior/felipe/dominioorm/DominioormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.felipe.dominioorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
