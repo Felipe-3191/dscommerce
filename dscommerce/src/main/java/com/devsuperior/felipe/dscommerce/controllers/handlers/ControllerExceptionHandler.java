a20f304aea200e9377650df412bb60d054065c29
Felipe Santos <felipe.dss17@gmail.com>
2023-06-13 20:08:01 -0300

    criação de exceção para exibir os erros do BeanValidation

--- dscommerce/src/main/java/com/devsuperior/felipe/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ dscommerce/src/main/java/com/devsuperior/felipe/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.devsuperior.felipe.dscommerce.controllers.handlers;
 
 import com.devsuperior.felipe.dscommerce.dto.CustomError;
+import com.devsuperior.felipe.dscommerce.dto.ValidationError;
 import com.devsuperior.felipe.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.felipe.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -29,6 +32,15 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "erros de validação", request.getRequestURI());
+        e.getBindingResult().getFieldErrors().forEach(er ->
+           err.addError(er.getField(),er.getDefaultMessage()));
+        return ResponseEntity.status(status).body(err);
+    }
   /*  @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<CustomError> databaseException(DataIntegrityViolationException ex, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
---
+++ dscommerce/src/main/java/com/devsuperior/felipe/dscommerce/dto/ValidationError.java
@@ -0,0 +1,24 @@
+package com.devsuperior.felipe.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErrors(){
+        return Collections.unmodifiableList(errors);
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add( new FieldMessage(fieldName, message));
+    }
+}
