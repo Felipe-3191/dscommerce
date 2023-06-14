71a3216e301a53924b0573a531b890a61b03888c
Felipe Santos <felipe.dss17@gmail.com>
2023-06-13 21:08:32 -0300

    criação da estrutura básica do projeto

---
+++ desafio3-CRUD/desafiocrud/src/test/java/com/devsuperior/felipe/desafiocrud/DesafiocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.felipe.desafiocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
