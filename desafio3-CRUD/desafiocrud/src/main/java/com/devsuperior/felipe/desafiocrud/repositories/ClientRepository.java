cdad830abac8db436d1b452ed1541910cc194a0a
Felipe Santos <felipe.dss17@gmail.com>
2023-08-01 21:15:30 -0300

    limpando repo para deixar apenas o dscommerce

--- desafio3-CRUD/desafiocrud/src/main/java/com/devsuperior/felipe/desafiocrud/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.felipe.desafiocrud.repositories;
-
-import com.devsuperior.felipe.desafiocrud.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository <Client, Long> {
-}
--- desafio3-CRUD/desafiocrud/src/main/java/com/devsuperior/felipe/desafiocrud/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.felipe.desafiocrud.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
