e2193261eb1dbab4387dea686e5f270f45663fd0
Felipe Santos <felipe.dss17@gmail.com>
2023-08-04 21:04:41 -0300

    adicionando Spring Security e liberando acesso ao h2 no perfil de teste

---
+++ src/main/java/com/devsuperior/felipe/dscommerce/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.devsuperior.felipe.dscommerce.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @Profile("test")
+    @Order(1)
+    public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
+
+        return http.build();
+
+    }
+}
